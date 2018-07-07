625cc22f4e392905d169b7ff4457673333f1326b
Lars Johan <larsjny@stud.ntnu.no>
2018-07-07 02:25:18 +0200

    #2 Added UI-Panel to control addon. Added operators

---
+++ ui.py
@@ -0,0 +1,18 @@
+import bpy
+from .operators import start, stop
+
+
+class VRPanel(bpy.types.Panel):
+    bl_idname = "VIEW3D_PT_BlenderVR"
+    bl_label = "BlenderVR"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    @classmethod
+    def poll(cls, context):
+        return context.object is not None
+
+    def draw(self, context):
+        self.layout.operator("vr.activate", text="Start VR")
+        self.layout.operator("vr.deactivate", text="Stop VR")
+
